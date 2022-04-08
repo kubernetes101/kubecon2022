178fd768661cef6eeca5cd8cbce13d53eb7f55e0
Tiffany Wang <5248989+tiffanywang3@users.noreply.github.com>
2022-04-08 14:56:02 +0100

    Add Flux and /deploy via GitOps

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -9,5 +9,18 @@ echo "$(date)    post-create start" >> "$HOME/status"
 git -C /workspaces/imdb-app pull
 git -C /workspaces/webvalidate pull
 
+# install Flux
+flux install >> ~/status
+
+# create Flux GitRepository
+flux create source git "${organization,,}-${repository,,}" \
+    --url https://github.com/${organization}/${repository} \
+    --branch main \
+    --username=PersonalAccessToken \
+    --password=${GITHUB_TOKEN} >> ~/status
+
+# create Flux Kustomization
+flux create kustomization "${organization,,}-${repository,,}" --source GitRepository/"${organization,,}-${repository,,}" --path="${deploymentPath}" --prune=true --interval=1m >> ~/status
+
 echo "post-create complete"
 echo "$(date +'%Y-%m-%d %H:%M:%S')    post-create complete" >> "$HOME/status"
