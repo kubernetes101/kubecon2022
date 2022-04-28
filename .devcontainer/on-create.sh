b0edb91c4996fc3c394506c1ecdc9d79cd3a78a7
Joaquin Rodriguez <rjoaquin@microsoft.com>
2022-04-28 12:40:16 -0400

    Refactor docker images

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -6,7 +6,6 @@ echo "on-create start"
 echo "$(date +'%Y-%m-%d %H:%M:%S')    on-create start" >> "$HOME/status"
 
 # clone repos
-git clone https://github.com/cse-labs/imdb-app /workspaces/imdb-app
 git clone https://github.com/microsoft/webvalidate /workspaces/webvalidate
 
 export REPO_BASE=$PWD
@@ -23,28 +22,19 @@ mkdir -p "$HOME/.oh-my-zsh/completions"
     echo "compinit"
 } >> "$HOME/.zshrc"
 
-# restore the repos
-dotnet restore /workspaces/webvalidate/src/webvalidate.sln
-dotnet restore /workspaces/imdb-app/src/imdb.csproj
-
 # make sure everything is up to date
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get autoremove -y
 sudo apt-get clean -y
 
-# create local registry
-docker network create k3d
-k3d registry create registry.localhost --port 5500
-docker network connect k3d k3d-registry.localhost
-
 # update the base docker images
-docker pull mcr.microsoft.com/dotnet/sdk:5.0-alpine
-docker pull mcr.microsoft.com/dotnet/aspnet:5.0-alpine
-docker pull mcr.microsoft.com/dotnet/sdk:5.0
-docker pull mcr.microsoft.com/dotnet/aspnet:6.0-alpine
-docker pull mcr.microsoft.com/dotnet/sdk:6.0
-docker pull ghcr.io/cse-labs/webv-red:latest
+# docker pull mcr.microsoft.com/dotnet/sdk:5.0-alpine
+# docker pull mcr.microsoft.com/dotnet/aspnet:5.0-alpine
+# docker pull mcr.microsoft.com/dotnet/sdk:5.0
+# docker pull mcr.microsoft.com/dotnet/aspnet:6.0-alpine
+# docker pull mcr.microsoft.com/dotnet/sdk:6.0
+docker pull ghcr.io/kubernetes101/webv-red:latest
 
 echo "generating kic completion"
 kic completion zsh > "$HOME/.oh-my-zsh/completions/_kic"
--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -6,7 +6,6 @@ echo "post-create start"
 echo "$(date)    post-create start" >> "$HOME/status"
 
 # update the repos
-git -C /workspaces/imdb-app pull
 git -C /workspaces/webvalidate pull
 
 # install Flux
