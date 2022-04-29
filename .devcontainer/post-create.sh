87fc42c911285fcf381782242ac5b385d5a4acd5
Joaquin Rodriguez <rjoaquin@microsoft.com>
2022-04-29 13:50:06 +0000

    Removed unused references

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -5,9 +5,6 @@
 echo "post-create start"
 echo "$(date)    post-create start" >> "$HOME/status"
 
-# update the repos
-git -C /workspaces/webvalidate pull
-
 # install Flux
 flux install >> ~/status
 
--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -6,11 +6,6 @@ echo "post-start start"
 echo "$(date +'%Y-%m-%d %H:%M:%S')    post-start start" >> "$HOME/status"
 
 # update the base docker images
-# docker pull mcr.microsoft.com/dotnet/sdk:5.0-alpine
-# docker pull mcr.microsoft.com/dotnet/aspnet:5.0-alpine
-# docker pull mcr.microsoft.com/dotnet/sdk:5.0
-# docker pull mcr.microsoft.com/dotnet/aspnet:6.0-alpine
-# docker pull mcr.microsoft.com/dotnet/sdk:6.0
 docker pull ghcr.io/kubernetes101/webv-red:latest
 
 echo "post-start complete"
