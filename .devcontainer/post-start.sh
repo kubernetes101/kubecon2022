ce354bb6ecbc4657da758590d6456f7767cd7ab7
Joaquin Rodriguez <rjoaquin@microsoft.com>
2022-04-29 14:08:08 +0000

    Updated image references

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -7,6 +7,11 @@ echo "$(date +'%Y-%m-%d %H:%M:%S')    post-start start" >> "$HOME/status"
 
 # update the base docker images
 docker pull ghcr.io/kubernetes101/webv-red:latest
+docker pull ghcr.io/kubernetes101/heartbeat:latest
+docker pull ghcr.io/kubernetes101/imdb-app:latest
+docker pull ghcr.io/kubernetes101/fluent-bit:1.5
+docker pull ghcr.io/kubernetes101/grafana:8.1.1
+docker pull ghcr.io/kubernetes101/prometheus:v2.29.1
 
 echo "post-start complete"
 echo "$(date +'%Y-%m-%d %H:%M:%S')    post-start complete" >> "$HOME/status"
